3aa0932b3b0cd41c11b6e32be6e3a454d81d644a
Jinkxx <everythinginreturninfo.com>
2026-08-18 16:40:13 -0400

    Final updates to the modTheCube project before deploying it as a webGL project on Unity Play. All challenges completed. Extra challenges completed with smooth transitions via mathf pinpong use

--- Assets/ModTheCube/Cube.cs
+++ Assets/ModTheCube/Cube.cs
@@ -6,17 +6,17 @@ public class Cube : MonoBehaviour
 {
     public MeshRenderer Renderer;    
     float rotationSpeed;
+    float changexyz;
+    float changeSize;
     void Start()
     {
-        rotationSpeed = Random.Range(1.5f, 15.0f);
-        //position xyz
-        transform.position = new Vector3(3, 5, 1);
-        //size
-        transform.localScale = Vector3.one * 1.9f;
+
     }
     
     void Update()
     {
+        transform.localScale = Vector3.one * changeSize;
+        transform.position = new Vector3(changexyz, changexyz, changexyz);
         transform.Rotate(0.0f,10.0f * Time.deltaTime * rotationSpeed, 0.0f);
         //change color over time using unitys time.time
         //rgba color, remember a controls opacity, 0 is fully transparent, 1 is fully opaque
@@ -26,5 +26,9 @@ public class Cube : MonoBehaviour
         float b = Mathf.PingPong(Time.time * 0.7f, 1.0f);
         float a = Mathf.PingPong(Time.time * 0.2f, 1.0f);
         material.color = new Color(r, g, b, a);
+        rotationSpeed = Mathf.PingPong(Time.time * 0.5f, 100.0f);
+        changexyz = Mathf.PingPong(Time.time * 0.5f, 5.0f);
+        changeSize = Mathf.PingPong(Time.time * 0.5f, 2.0f) + 0.5f;
+        
     }
 }
