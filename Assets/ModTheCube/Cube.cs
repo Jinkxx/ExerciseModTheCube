927698dc15f25f69b94d36751639be60c9373a0d
Jinkxx <everythinginreturninfo.com>
2026-08-18 14:05:22 -0400

    finished 2 last challenges by implemented randomizers and using Update to continuously change the colors

--- Assets/ModTheCube/Cube.cs
+++ Assets/ModTheCube/Cube.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class Cube : MonoBehaviour
 {
-    public MeshRenderer Renderer;
-    
+    public MeshRenderer Renderer;    
+    float rotationSpeed;
     void Start()
     {
+        rotationSpeed = Random.Range(1.5f, 15.0f);
         //position xyz
         transform.position = new Vector3(3, 5, 1);
         //size
         transform.localScale = Vector3.one * 1.9f;
-        
-        Material material = Renderer.material;
-        //rgba color
-        material.color = new Color(2.6f, 1.4f, 0.6f, 0.3f);
     }
     
     void Update()
     {
-        transform.Rotate(0.0f,10.0f * Time.deltaTime * 10, 0.0f);
+        transform.Rotate(0.0f,10.0f * Time.deltaTime * rotationSpeed, 0.0f);
+        //change color over time using unitys time.time
+        //rgba color, remember a controls opacity, 0 is fully transparent, 1 is fully opaque
+        Material material = Renderer.material;
+        float r = Mathf.PingPong(Time.time * 0.5f, 1.0f);
+        float g = Mathf.PingPong(Time.time * 0.3f, 1.0f);
+        float b = Mathf.PingPong(Time.time * 0.7f, 1.0f);
+        float a = Mathf.PingPong(Time.time * 0.2f, 1.0f);
+        material.color = new Color(r, g, b, a);
     }
 }
